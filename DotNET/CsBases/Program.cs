f59885e2c0dc15e86738ac2c673467ae322b2182
DarkCastro24 <104780226+DarkCastro24@users.noreply.github.com>
2025-10-26 23:36:47 -0600

    add: basic structure

---
+++ DotNET/CsBases/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program()
+{
+    static void Main()
+    {
+        int quantity = 5;
+        string message = "Hello, World!";
+        decimal price = 19.99m;
+        Console.WriteLine($"Message: {message}, Quantity: {quantity}, Price: {price:C}");
+        // Declaracion con var 
+        // Adivina el tipo de dato
+        var greeting = "Hola"; // infiere que es string
+        var porcentage = 99.9; // infiere que es double
+        Console.WriteLine($"Greeting: {greeting}, Porcentage: {porcentage}");
+    }
+}
